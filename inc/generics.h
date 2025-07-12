b48415ed8095ecfd576cb38f41445529715582a8
hwang-fu <junzhe.hwangfu@gmail.com>
2025-07-12 19:32:39 +0200

    feat: add customized types

--- inc/generics.h
+++ inc/generics.h
@@ -1,4 +1,22 @@
 #ifndef GENESIS_GENERICS_H
 #define GENESIS_GENERICS_H
 
+typedef float           f32 ;
+typedef double          f64 ;
+
+typedef char            i8  ;
+typedef unsigned char   u8  ;
+
+typedef short           i16 ;
+typedef unsigned short  u16 ;
+
+typedef int             i32 ;
+typedef unsigned int    u32 ;
+
+typedef long            i64 ;
+typedef unsigned long   u64 ;
+
+#define nil (0)
+
+
 #endif // GENESIS_GENERICS_H
