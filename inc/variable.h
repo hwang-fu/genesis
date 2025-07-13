93d98fc2578154cabda402119f7d04ce0df4c91a
hwang-fu <junzhe.hwangfu@gmail.com>
2025-07-14 00:09:13 +0200

    feat: new implementation for variable

--- inc/variable.h
+++ inc/variable.h
@@ -1,4 +1,35 @@
 #ifndef GENESIS_VARIABLE_H
 #define GENESIS_VARIABLE_H
 
+#include "generics.h"
+#include "type.h"
+
+typedef enum TVariable TVariable;
+enum TVariable {
+    VAR_LOCAL,
+    VAR_GLOBAL,
+    VAR_ENUMER,
+};
+
+typedef struct Variable Variable;
+struct Variable {
+    copy     TVariable tag;
+    owned    char *    name;
+    borrowed Type *    type;
+    copy     i64       offset;
+    copy     u64       enumer;
+};
+typedef Variable Parameter;
+
+owned Variable * var_new();
+owned Variable * lvar_new();
+owned Variable * lvar_ctor(borrowed const char * name, borrowed Type * type, copy i64 offset);
+owned Variable * lvar_ctor_owned(owned char * name, borrowed Type * type, copy i64 offset);
+owned Variable * gvar_new();
+owned Variable * gvar_ctor(borrowed const char * name, borrowed Type * type);
+owned Variable * gvar_ctor_owned(owned char * name, borrowed Type * type);
+
+void var_dtor(borrowed void * arg);
+void var_destroy(owned void * arg);
+
 #endif // GENESIS_VARIABLE_H
--- src/variable.c
+++ src/variable.c
@@ -1,3 +1,82 @@
+#include <stdlib.h>
 
 #include "variable.h"
+#include "generics.h"
+#include "utility.h"
+
+owned Variable * var_new() {
+    owned Variable * var = new(sizeof(Variable));
+    var->tag             = VAR_GLOBAL;
+    var->name            = nil;
+    var->type            = nil;
+    var->offset          = 0;
+    var->enumer          = 0;
+    return var;
+}
+
+owned Variable * lvar_new() {
+    owned Variable * var = new(sizeof(Variable));
+    var->tag             = VAR_LOCAL;
+    var->name            = nil;
+    var->type            = nil;
+    var->offset          = 0;
+    var->enumer          = 0;
+    return var;
+}
+
+owned Variable * lvar_ctor(borrowed const char * name, borrowed Type * type, copy i64 offset) {
+    owned Variable * lvar = lvar_new();
+    lvar->name            = s_copy(name);
+    lvar->type            = type;
+    lvar->offset          = offset;
+    return lvar;
+}
+
+owned Variable * lvar_ctor_owned(owned char * name, borrowed Type * type, copy i64 offset) {
+    owned Variable * lvar = lvar_new();
+    lvar->name            = name;
+    lvar->type            = type;
+    lvar->offset          = offset;
+    return lvar;
+}
+
+owned Variable * gvar_new() {
+    owned Variable * var = new(sizeof(Variable));
+    var->tag             = VAR_GLOBAL;
+    var->name            = nil;
+    var->type            = nil;
+    var->offset          = 0;
+    var->enumer          = 0;
+    return var;
+}
+
+owned Variable * gvar_ctor(borrowed const char * name, borrowed Type * type) {
+    owned Variable * gvar = gvar_new();
+    gvar->name            = s_copy(name);
+    gvar->type            = type;
+    return gvar;
+}
+
+owned Variable * gvar_ctor_owned(owned char * name, borrowed Type * type) {
+    owned Variable * gvar = gvar_new();
+    gvar->name            = name;
+    gvar->type            = type;
+    return gvar;
+}
+
+void var_dtor(borrowed void * arg) {
+    if (!arg) {
+        return;
+    }
+    borrowed Variable * var = cast(arg, Variable*);
+    sfree(var->name);
+}
+
+void var_destroy(owned void * arg) {
+    if (!arg) {
+        return;
+    }
+    var_dtor(arg);
+    free(arg);
+}
 
